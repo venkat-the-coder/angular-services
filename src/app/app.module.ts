91809ac36fa1013a7a15ae86846cbdddb25bc4da
venkat <venkat.prabhu@10decoders.in>
2024-04-15 10:04:26 +0530

    created basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddReaderComponent } from './add-reader/add-reader.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { EditReaderComponent } from './edit-reader/edit-reader.component';
+import { RootRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddReaderComponent,
+    AddBookComponent,
+    EditBookComponent,
+    EditReaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RootRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/book.ts
@@ -0,0 +1,6 @@
+export class Book {
+    bookID: number | undefined;
+    title: string  | undefined;
+    author: string  | undefined;
+    publicationYear: number  | undefined;
+}
