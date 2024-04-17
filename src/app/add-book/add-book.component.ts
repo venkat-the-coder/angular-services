4706366e4704f5cbea32fa0fa5c6506a9f2c167c
venkat <venkat.prabhu@10decoders.in>
2024-04-17 11:43:31 +0530

    async/await no call back needed using try/catch

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -19,9 +19,18 @@ export class AddBookComponent implements OnInit {
       (err: string) => console.log(err)
     );
 
+    // the above can also be written in the following way using async and await
     this.dataService.getAllReaders().subscribe(
       (data: Reader[] | Error) => (this.readers = <Reader[]>data),
       (err: Error) => console.log(err)
     );
+  }
+
+  private async getAuthorRecomendAsync(id: number): Promise<void> {
+    try {
+      let result: string = await this.dataService.getAuthorRecommendations(id);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
